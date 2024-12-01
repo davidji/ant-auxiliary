2b3eb0b410edbed65bfda3080ebae5c1d3b56f72
David Ireland <davidji@pobox.com>
2024-12-01 16:13:00 +0000

    blinks and USB device appears

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# Extra functionality for the Ant PCB Maker
+
+I need
+
+ - A USB to serial adapter
+ - Fan PWM control
+ - OnWire temperature sensor monitoring
+ - Fan and spindle RPM monitoring
+
+I have a STM32F103 Black Pill, which should be able to do all that, so this project
+is to write a firmware that can do the above on it.
+
+I've gone for Rust and RTIC. When I first started messing around with embedded Rust,
+I was impressed by how joined up it all was. It's a bit messier at the moment -
+the splitting out of embedded-io and embedded-nb is not something the various
+hals have cought up with just yet, but I suspect that's temporary.
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@
 use panic_semihosting as _;
 use cortex_m::asm::delay;
 use stm32f1xx_hal::{
-    gpio::{gpioa::{self, PA0}, Alternate }, 
+    gpio::{gpioa::{self, PA0}, gpiob::PB12, Alternate, Output }, 
     pac,
     prelude::*,
     rcc,
@@ -17,14 +17,23 @@ use stm32f1xx_hal::{
 };
 use usb_device::prelude::*;
 use usbd_serial::embedded_io::{ReadReady, WriteReady};
+use rtic_sync::{
+    channel::*,
+    make_channel
+};
+use rtic_monotonics::systick::prelude::*;
+systick_monotonic!(Mono, 1_000);
 
 type GrblTx = serial::Tx1;
 type GrblRx = serial::Rx1;
 
+const SERIAL_CHANNEL_CAPACITY: usize = 16;
+type SerialChannelSender = Sender<'static, u8, SERIAL_CHANNEL_CAPACITY>;
+type SerialChannelReceiver = Receiver<'static, u8, SERIAL_CHANNEL_CAPACITY>;
+
 #[rtic::app(device = stm32f1xx_hal::pac, dispatchers = [SPI3, UART4, UART5, TIM6, TIM7])]
 mod app {
     use super::*;
-
 
     #[shared]
     struct Shared {
@@ -36,15 +45,22 @@ mod app {
     struct Local {
         grbl_tx: GrblTx, 
         grbl_rx: GrblRx,
+        grbl_tx_sender: SerialChannelSender,
+        grbl_tx_receiver: SerialChannelReceiver,
+        grbl_rx_sender: SerialChannelSender,
+        grbl_rx_receiver: SerialChannelReceiver,
+
         fan_pwm: PwmHz<pac::TIM2, Tim2NoRemap, Ch<0>, PA0<Alternate>>,
+        led: PB12<Output>,
     }
 
     #[init(local=[usb_bus:  Option<usb_device::bus::UsbBusAllocator<UsbBusType>> = None])]
-    fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
+    fn init(cx: init::Context) -> (Shared, Local) {
 
         let peripherals = cx.device;
         let mut flash = peripherals.FLASH.constrain();
         let rcc = peripherals.RCC.constrain();
+      
 
         let clocks: rcc::Clocks = rcc
             .cfgr
@@ -75,20 +91,7 @@ mod app {
 
 
         let usb_bus = cx.local.usb_bus.as_ref().unwrap();
-
         let usb_serial = usbd_serial::SerialPort::new(usb_bus);
-
-        let usb_dev = UsbDeviceBuilder::new(
-            usb_bus,
-            UsbVidPid(0x16c0, 0x27dd),
-        )
-        .device_class(usbd_serial::USB_CLASS_CDC)
-        .strings(&[StringDescriptors::default()
-            .manufacturer("paraxial")
-            .product("ant pcb maker")
-            .serial_number("grbl")])
-        .unwrap()
-        .build();
 
         let (grbl_tx, grbl_rx) = grbl_serial(
             peripherals.USART1, 
@@ -104,83 +107,143 @@ mod app {
             .pwm_hz::<Tim2NoRemap, _, _>(fan_pwm_pin, &mut afio.mapr, 25.kHz(), &clocks);
         fan_pwm.enable(Channel::C1);
 
-        (Shared { 
-            usb_dev, 
-            usb_serial }, 
+        let (grbl_tx_sender,grbl_tx_receiver) = make_channel!(u8, SERIAL_CHANNEL_CAPACITY);
+        let (grbl_rx_sender, grbl_rx_receiver) = make_channel!(u8, SERIAL_CHANNEL_CAPACITY);
+        
+        let mut gpiob = peripherals.GPIOB.split();
+
+        Mono::start(cx.core.SYST, 72_000_000);
+
+        blink::spawn().unwrap();
+        usb_poll::spawn().unwrap();
+        grbl_serial_poll::spawn().unwrap();
+
+    
+
+        (Shared {
+            usb_dev : grbl_usb_device(usb_bus), 
+            usb_serial,
+         }, 
         Local {
             grbl_tx,
             grbl_rx,
+            grbl_tx_sender,
+            grbl_tx_receiver,
+            grbl_rx_sender,
+            grbl_rx_receiver,
             fan_pwm,
-        }, init::Monotonics())
+            led: gpiob.pb12.into_push_pull_output(&mut gpiob.crh)
+        })
     }
 
-    #[task(binds = USB_HP_CAN_TX)]
-    fn usb_tx(_: usb_tx::Context) {
-        usb::spawn().unwrap();
+    #[task(local = [ led ])]
+    async fn blink(cx: blink::Context) {
+        loop {
+            Mono::delay(1000.millis()).await;
+            cx.local.led.toggle();
+        }
     }
 
-    #[task(binds = USB_LP_CAN_RX0)]
-    fn usb_rx0(_: usb_rx0::Context) {
-        usb::spawn().unwrap();
+    #[task(binds = USB_HP_CAN_TX, shared = [usb_dev, usb_serial])]
+    fn usb_hp(cx: usb_hp::Context) {
+        let dev = cx.shared.usb_dev;
+        let serial = cx.shared.usb_serial;
+        if (dev, serial).lock(|dev,serial| {
+            dev.poll(&mut [serial]) 
+        }) {
+            // Just ignore the error: it's because the task is already running
+           _ = usb::spawn();
+        }
     }
 
-    #[task(shared = [usb_dev, usb_serial])]
-    fn usb(cx: usb::Context) {
-        let mut usb_dev = cx.shared.usb_dev;
-        let mut serial = cx.shared.usb_serial;
+    #[task(binds = USB_LP_CAN_RX0, shared = [usb_dev, usb_serial])]
+    fn usb_lp(cx: usb_lp::Context) {
+        let dev = cx.shared.usb_dev;
+        let serial = cx.shared.usb_serial;
+        if (dev, serial).lock(|dev,serial| {
+            dev.poll(&mut [serial]) 
+        }) {
+            // Just ignore the error: it's because the task is already running
+           _ = usb::spawn();
+        }
+    }
 
-        (&mut usb_dev, &mut serial).lock(|usb_dev, serial| {
-            if usb_dev.poll(&mut [serial]) {
-                if serial.read_ready().unwrap() {
-                    grbl_serial_tx::spawn().unwrap();
-                }
+    #[task]
+    async fn usb_poll(_: usb_poll::Context) {
+        loop {
+            Mono::delay(1.millis()).await;
+            _ = usb::spawn();
+        }
+    }
 
-                if serial.write_ready().unwrap() {
-                    grbl_serial_rx::spawn().unwrap();
-                }                
+    #[task(shared = [usb_dev, usb_serial], local = [ grbl_tx_sender, grbl_rx_receiver], priority = 3)]
+    async fn usb(cx: usb::Context) {
+        let dev = cx.shared.usb_dev;
+        let serial = cx.shared.usb_serial;
+        let tx_sender = cx.local.grbl_tx_sender;
+        let rx_receiver = cx.local.grbl_rx_receiver;
+        (dev, serial).lock(|dev,serial| {
+            while dev.poll(&mut [serial]) {
+                usb_serial_io(serial, tx_sender, rx_receiver);
             }
         });
     }
 
     #[task(binds = USART1)] 
     fn grbl_serial_interrupt(_: grbl_serial_interrupt::Context) {
-        grbl_serial_rx::spawn().unwrap();
-        grbl_serial_tx::spawn().unwrap();
+        _ = grbl_serial_rx::spawn();
+        _ = grbl_serial_tx::spawn();
     }
-
-    #[task(local=[grbl_rx], shared = [usb_serial])]
-    fn grbl_serial_rx(mut cx: grbl_serial_rx::Context) {
-        let grbl_rx = cx.local.grbl_rx;
-        if grbl_rx.is_rx_not_empty() {
-            cx.shared.usb_serial.lock(|usb_serial| {
-                while grbl_rx.is_rx_not_empty() && usb_serial.write_ready().unwrap() {
-                    let buf = [grbl_rx.read().unwrap()];
-                    usb_serial.write(&buf).unwrap();
-                }
-            })
+ 
+    #[task]
+    async fn grbl_serial_poll(_: grbl_serial_poll::Context) {
+        loop {
+            Mono::delay(10.millis()).await;
+            _ = grbl_serial_rx::spawn();
+            _ = grbl_serial_tx::spawn();
         }
     }
 
-    #[task(local=[grbl_tx], shared = [usb_serial])]
-    fn grbl_serial_tx(mut cx: grbl_serial_tx::Context) {
-        let grbl_tx: &mut serial::Tx<you_must_enable_the_rt_feature_for_the_pac_in_your_cargo_toml::USART1> = cx.local.grbl_tx;
-        if grbl_tx.is_tx_empty() {
-            cx.shared.usb_serial.lock(|usb_serial: &mut usbd_serial::SerialPort<'_, UsbBus<Peripheral>>| {
-                while usb_serial.read_ready().unwrap() && grbl_tx.is_tx_empty() {
-                    let mut buf = [0u8; 1];
-                    usb_serial.read(&mut buf).unwrap();
-                    grbl_tx.write(buf[0]).unwrap();
-                }
-            });
+    #[task(local=[grbl_rx, grbl_rx_sender], priority=2)]
+    async fn grbl_serial_rx(cx: grbl_serial_rx::Context) {
+        let grbl_rx = cx.local.grbl_rx;
+        loop {
+            match grbl_rx.read() {
+                Ok(data) => cx.local.grbl_rx_sender.send(data).await.unwrap(),
+                Err(nb::Error::WouldBlock) => break,
+                Err(_) => panic!("Error reading from GRBL serial")
+            }
+        }
+    }
+
+    #[task(local=[grbl_tx, grbl_tx_receiver], priority=2)]
+    async fn grbl_serial_tx(cx: grbl_serial_tx::Context) {
+        let grbl_tx = cx.local.grbl_tx;
+        while grbl_tx.is_tx_empty() {
+            let data = cx.local.grbl_tx_receiver.recv().await.unwrap();
+            grbl_tx.write(data).unwrap();
         }
     }
 
     #[task(local=[fan_pwm])]
-    fn set_fan_speed(cx: set_fan_speed::Context) {
-
+    async fn set_fan_speed(cx: set_fan_speed::Context, duty: u16) {
+        cx.local.fan_pwm.set_duty(Channel::C1, duty);
     }
 }
 
+fn grbl_usb_device(usb_bus: &usb_device::bus::UsbBusAllocator<UsbBus<Peripheral>>) -> UsbDevice<'_, UsbBus<Peripheral>> {
+    UsbDeviceBuilder::new(
+        usb_bus,
+        UsbVidPid(0x16c0, 0x27dd),
+    )
+    .device_class(usbd_serial::USB_CLASS_CDC)
+    .strings(&[StringDescriptors::default()
+        .manufacturer("paraxial")
+        .product("ant pcb maker")
+        .serial_number("grbl")])
+    .unwrap()
+    .build()
+}
 
 fn grbl_serial(
     usart: pac::USART1,
@@ -199,4 +262,27 @@ fn grbl_serial(
           &clocks);
 
     return grbl_serial.split();
+}
+
+fn usb_serial_io(
+    serial: &mut usbd_serial::SerialPort<'static, UsbBusType>, 
+    sender: &mut SerialChannelSender, 
+    receiver: &mut SerialChannelReceiver) {
+        
+    while serial.read_ready().unwrap() && !sender.is_full() {
+         let mut buf = [0u8; 1];
+         match serial.read(&mut buf) {
+            Ok(_) => sender.try_send(buf[0]).unwrap(),
+            Err(UsbError::WouldBlock) => break,
+            Err(_) => panic!("Error writing to USB serial"),
+        };
+    }
+
+    while serial.write_ready().unwrap() {
+        match receiver.try_recv() {
+            Ok(data) => assert!(serial.write(&[data]).is_ok()),
+            Err(ReceiveError::Empty) => break,
+            Err(ReceiveError::NoSender) => break
+        }
+    }
 }
